e9232e52cff7ebedaebaddf1ca0352b75dc49400
jeonghee <wjd-gml078@naver.com>
2021-08-13 19:56:29 +0900

    jung

--- app/src/main/java/com/example/InNayo/Congestion.java
+++ app/src/main/java/com/example/InNayo/Congestion.java
@@ -1,14 +1,37 @@
 package com.example.InNayo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Congestion extends AppCompatActivity {
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+
+
+public class Congestion extends AppCompatActivity implements OnMapReadyCallback {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congestion);
+
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
     }
 }
