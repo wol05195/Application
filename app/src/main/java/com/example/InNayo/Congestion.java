331b61b36dd99404b4bc350abbce9128e5a0a196
jeonghee <wjd-gml078@naver.com>
2021-08-26 17:42:10 +0900

    jeongheeeeeee

--- app/src/main/java/com/example/InNayo/Congestion.java
+++ app/src/main/java/com/example/InNayo/Congestion.java
@@ -3,11 +3,16 @@ package com.example.InNayo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
 
 
 public class Congestion extends AppCompatActivity implements OnMapReadyCallback {
@@ -32,6 +37,21 @@ public class Congestion extends AppCompatActivity implements OnMapReadyCallback 
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.55834853256298, 127.04990932947216));
+        marker.setMap(naverMap);
+        marker.setWidth(100);
+        marker.setHeight(100);
+        marker.setIcon(OverlayImage.fromResource(R.drawable.h));
+        marker.setCaptionText("한양여자대학교");
+        marker.setCaptionRequestedWidth(200);
+
+
+
+
+
+
 
     }
 }
