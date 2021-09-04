002d7f571e1d364158730bf3cb030be5ef0f78b5
mohanhee <mohanhee@naver.com>
2021-09-04 20:05:37 +0900

    update 9/4 20:05

--- app/src/main/java/com/example/InNayo/Main.java
+++ app/src/main/java/com/example/InNayo/Main.java
@@ -11,7 +11,8 @@ import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
     Intent intent;
-    Button main_button1, main_button2, main_button3, main_button4, main_button5;
+    Button main_button1, main_button2;
+    TextView main_tv1, main_tv2, main_tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +21,9 @@ public class Main extends AppCompatActivity {
 
         main_button1 = (Button) findViewById(R.id.main_button1);
         main_button2 = (Button) findViewById(R.id.main_button2);
-        main_button3 = (Button) findViewById(R.id.main_button3);
-        main_button4 = (Button) findViewById(R.id.main_button4);
-        main_button5 = (Button) findViewById(R.id.main_button5);
+        main_tv1 = (TextView) findViewById(R.id.main_tv1);
+        main_tv2 = (TextView) findViewById(R.id.main_tv2);
+        main_tv3 = (TextView) findViewById(R.id.main_tv3);
 
         Button.OnClickListener onClickListener = new Button.OnClickListener(){
             @Override
@@ -36,26 +37,42 @@ public class Main extends AppCompatActivity {
                         intent = new Intent(getApplicationContext(), Booking.class);
                         startActivity(intent);
                         break;
-                    case R.id.main_button3:
-                        intent = new Intent(getApplicationContext(), Login.class);
-                        startActivity(intent);
-                        break;
-                    case R.id.main_button4:
-                        intent = new Intent(getApplicationContext(), Signup.class);
-                        startActivity(intent);
-                        break;
-                    case R.id.main_button5:
-                        intent = new Intent(getApplicationContext(), Administrator.class);
-                        startActivity(intent);
-                        break;
+
                 }
             }
         };
+
+        TextView main_tv1 = findViewById(R.id.main_tv1);
+        main_tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView main_tv2 = findViewById(R.id.main_tv2);
+        main_tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView main_tv3 = findViewById(R.id.main_tv3);
+        main_tv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Administrator.class);
+                startActivity(intent);
+            }
+        });
+
+
         main_button1.setOnClickListener(onClickListener);
         main_button2.setOnClickListener(onClickListener);
-        main_button3.setOnClickListener(onClickListener);
-        main_button4.setOnClickListener(onClickListener);
-        main_button5.setOnClickListener(onClickListener);
+
 
     }
 }
