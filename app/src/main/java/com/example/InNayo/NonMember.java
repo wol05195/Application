3adfb765e5247e472563772d948655c9f7ee5d8a
wol05195 <wol05195@naver.com>
2021-11-16 02:10:47 +0900

    2021-11-16 min

--- app/src/main/java/com/example/InNayo/NonMember.java
+++ app/src/main/java/com/example/InNayo/NonMember.java
@@ -2,7 +2,11 @@ package com.example.InNayo;
 
 import android.Manifest;
 import androidx.fragment.app.Fragment;
+
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -29,23 +33,34 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Iterator;
+
+import static com.example.InNayo.Reservation.urls;
 
 public class NonMember extends Fragment {
     Button non_member_bt;
     EditText fname, ftel, fpw, nonmember_et4;
     String value="";
     ArrayList<String> nonmember;
+    Context nonmembercontext;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.activity_non_member,container,false);
+
+        nonmembercontext = container.getContext();
 
         checkSelfPermission();
         readExcel();
@@ -70,6 +85,7 @@ public class NonMember extends Fragment {
                         createExcel();
                         updateExcel(Name, Tel, Password);
                     }
+                        insertToDatabase(Name, Tel);
                     Toast.makeText(getContext(), "비회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
                 }else {
                     Toast.makeText(getContext(), "비밀번호를 동일하게 입력해주세요.", Toast.LENGTH_SHORT).show();
@@ -201,6 +217,63 @@ public class NonMember extends Fragment {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void insertToDatabase(String Name, String Tel) {
+        class InsertData extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(nonmembercontext, "Please Wait", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+//                Toast toast = Toast.makeText(nonmembercontext, s, Toast.LENGTH_LONG);
+//                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 30);
+//                toast.show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    String Name = (String) params[0];
+                    String Tel = (String) params[1];
+
+                    String link = urls+"Register_non.php";
+                    String data = URLEncoder.encode("Name", "UTF-8") + "=" + URLEncoder.encode(Name, "UTF-8");
+                    data += "&" + URLEncoder.encode("Tel", "UTF-8") + "=" + URLEncoder.encode(Tel, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                } catch (Exception e) {
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+        InsertData task = new InsertData();
+        task.execute(Name, Tel);
     }
 
     @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
